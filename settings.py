c014ef8295c82a7a345127e0a18269c912fe4eb1
Ben Li <54297796+pleycothh@users.noreply.github.com>
2021-08-31 16:40:33 +1000

    init set up

---
+++ settings.py
@@ -0,0 +1,27 @@
+from pygame.math import Vector2 as vec
+
+#screen settings
+WIDTH, HEIGHT = 610, 670
+TP_BUFFER = 50
+
+MAZE_WIDTH, MAZE_HEIGHT = WIDTH - TP_BUFFER, HEIGHT - TP_BUFFER
+fps = 60
+
+
+#colour settings
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+BLUE = (170,130,60)
+YELLOW = (30,130,160)
+RED = (255,160,25)
+GRAY = (110,110,110)
+
+#font settings
+START_TEXT_SIZE = 16
+START_FONT = 'arial black'
+
+#player
+PLAYER_START_POS = vec(1,1)
+
+
+# bot settings
---
+++ test.py
@@ -0,0 +1,6 @@
+print('hello world')
+
+dec = 8454143
+bir = bin(dec)
+
+print(bir)
