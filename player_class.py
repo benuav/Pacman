f7eb1f84f5592c4f548d434ecb3573ee70c80fd7
Ben Li <54297796+pleycothh@users.noreply.github.com>
2021-09-01 13:06:05 +1000

    Update movement within grid
    add stored_dirction

--- player_class.py
+++ player_class.py
@@ -8,12 +8,24 @@ class Player():
         self.grid_pos = pos                       # the position based on the grid count
         self.pix_pos = self.get_pix_pos()         # use grid position, to gerate the current player position
         self.playerImg = pygame.image.load('sources/player.png')
-        self.dirction = vec(0,0)
+        self.dirction = vec(1,0)
         self.stored_dirction = None
+        print('dirction', self.dirction)
 
 
     def update(self):
+        print('dirction', self.dirction)
+
         self.pix_pos += self.dirction # final step to move the player
+
+        if int(self.pix_pos.x + TP_BUFFER//2) % self.app.cell_width == 0:
+            if self.dirction == vec(1,0) or self.dirction == vec(-1, 0):
+                if self.stored_dirction != None:
+                    self.dirction = self.stored_dirction
+        if int(self.pix_pos.y + TP_BUFFER//2) % self.app.cell_height == 0:
+            if self.dirction == vec(0,1) or self.dirction == vec(0, -1):
+                if self.stored_dirction != None:
+                    self.dirction = self.stored_dirction
 
         # give pix position to a grid position
         self.grid_pos[0] = (self.pix_pos[0] - TP_BUFFER + self.app.cell_width//2)//self.app.cell_width + 1
@@ -30,7 +42,7 @@ class Player():
                           self.app.cell_width, self.app.cell_height), 1)
 
     def move(self, dirction):
-        self.sotred_dirction = dirction
+        self.stored_dirction = dirction
 
     def get_pix_pos(self):
         return vec((self.grid_pos.x * self.app.cell_width)+ (self.app.cell_width + TP_BUFFER)//2,
